8728c926133dd855e7317c176e76e3956b6a8049
AppleBee12 <mieldeluna@naver.com>
2024-12-05 11:45:25 +0900

    📝 하위카테고리출력

--- header.php
+++ header.php
@@ -7,17 +7,20 @@
     <title>Minimal Portfolio</title>
   <?php wp_head(); ?>
 </head>
-<body>
+<body><?php body_class();?>
     <header>
        <h1 class="logo"><a href="">Minimal Portfolio Theme</a></h1>
        <nav>
-           <ul>
+           <!-- <ul>
                <li><a href="index.html">Home</a></li>
                <li><a href="portfolio.html">Portfolio</a></li>
                <li><a href="about.html">About</a></li>
                <li><a href="contact.html">Contact</a></li>
-           </ul>
-       </nav>              
+           </ul> -->
+           <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+       </nav>  
+       
+       
     </header>
     <hr>
     <main class="content">
